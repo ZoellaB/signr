6aacc92e67ed9c918b6c118c8308a98a7c099c6c
ZoellaB <zayced@gmail.com>
2025-03-17 03:39:57 -0700

    multiprocessing almost working

--- model/train.py
+++ model/train.py
@@ -6,14 +6,17 @@ import time
 import mediapipe as mp
 import json
 import sys
-from multiprocessing import Process
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
-
+from multiprocessing import Pool, Manager, Array
+# from sklearn.model_selection import train_test_split
+from itertools import product
+    
 mp_holistic = mp.solutions.holistic
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_drawing = mp.solutions.drawing_utils
-
+TRAIN_DIR = "training_data"
+GEN_DIR = 'gen_keypoints_data'
+START = 0
+KEYPTS_PATH = os.path.join(os.curdir, GEN_DIR)
 
 def mediapipe_detect(img, model):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -53,28 +56,27 @@ def get_keypts(results):
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
     return np.concatenate([pose, face, lh, rh])
 
-def get_keypts_orchestrator(start=0):
-    dir = "training_data"
-    KEYPTS_PATH = os.path.join(os.curdir, 'gen_keypoints_data')
-    if not os.path.exists(KEYPTS_PATH):
-        os.makedirs(KEYPTS_PATH)
-    word_count=0
-
-
-    #Save missing video instances
-    missing = open(dir + "/missing.txt").readlines()
-
+def get_word_classification(dir):
     #Save word classification from 0-1999 as dict
     word_class = {}
-    with open(dir + "/wlasl_class_list.txt") as class_file:
+    with open(TRAIN_DIR + dir) as class_file:
         for line in class_file:
             if (len(line.split(maxsplit=1)) != 2):
                 sys.exit("Error using wlasl class list")
             val, key = line.split(maxsplit=1) #class number is val, and name of word is key
             word_class[key.strip()] = str(val)
+    return word_class
 
+def get_missing_vids(miss_dir):
+    missing = []
+    #Save missing video instances
+    missing = open(TRAIN_DIR + miss_dir).readlines()
+    return missing
+
+def get_vid_metadata(dir):
+    train_list = []
     # Open the JSON file
-    with open(dir + '/WLASL_v0.3.json') as f:
+    with open(TRAIN_DIR + dir) as f:
         # Load the JSON data
         json_data = json.load(f)
     # Check if the data is a list of dictionaries
@@ -84,18 +86,27 @@ def get_keypts_orchestrator(start=0):
         
     else: # may break here.
         print("The JSON file does not contain an array.")
+
+    return train_list
+
+    
+
+def init_non_iter():
+    word_class = get_word_classification("/wlasl_class_list.txt")
+    missing = get_missing_vids("/missing.txt")
+    tl = get_vid_metadata("/WLASL_v0.3.json")
+    print("done making dicts lists")
+
+def get_keypts_orchestrator(train_list):
     # Trains per word, go to each 1+ instances
     for word in train_list:
-        if(word_count < start):
-            word_count += 1
-            continue
-        word_count += 1
         print("Training " + word['gloss'])
         for instance in word['instances']:
             try: 
                 os.makedirs(os.path.join(KEYPTS_PATH, word_class[str(word['gloss'])], str(instance['video_id'])))
             except:
                 pass
+
             if instance['video_id'] not in missing:
                 with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5) as holistic:
                     cap = cv2.VideoCapture('training_data/videos/' + instance['video_id'] +  '.mp4')
@@ -108,79 +119,38 @@ def get_keypts_orchestrator(start=0):
                         img, results = mediapipe_detect(frame, holistic)
                         frame_count += 1
 
-                        #Showing, not neccesary when training
-                        draw_landmarks(img, results)
-                        cv2.imshow('Cam Feed', img)
+                        # #Showing, not neccesary when training
+                        # draw_landmarks(img, results)
+                        # cv2.imshow('Cam Feed', img)
 
                         #Saving keypoints:
                         keypts = get_keypts(results)
                         numpy_path = os.path.join(KEYPTS_PATH, word_class[str(word['gloss'])], str(instance['video_id']),str(frame_count))
                         if not os.path.exists(numpy_path):
                             np.save(numpy_path, keypts)
-       
+        
                         if (cv2.waitKey(1) & 0xFF == ord('q')):
                             continue
                     cap.release()
-    
-    
-    
     cv2.destroyAllWindows()
+
+if not os.path.exists(KEYPTS_PATH):
+    os.makedirs(KEYPTS_PATH)
+    print("hey")
+
+def main():
+    wc = 
+    tl = Array('c', lock=False)
+
+    pool = Pool(initializer=init_non_iter)
     
+    print("way")
+    pool.imap_unordered(get_keypts_orchestrator, tl)
+    print("ney")
 
 if __name__ == '__main__':
-    p = Process(target=get_keypts_orchestrator) #stopped at 411 309 68 87 157 180 689 122 1228
-    p.start()
-    p.join()
+    main()
 
-
-
-# class_word = {}
-# with open("training_data" + "/wlasl_class_list.txt") as class_file:
-#     for line in class_file:
-#         if (len(line.split(maxsplit=1)) != 2):
-#             sys.exit("Error using wlasl class list")
-#         key, val = line.split(maxsplit=1) #class number is val, and name of word is key
-#         class_word[key.strip()] = str(val)
-
-# print(class_word)
 
 
     
-# sequences, labels = [], []
-# for action in actions:
-#     for sequence in range(no_sequences):
-#         window = []
-#         for frame_num in range(sequence_length):
-#             res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
-#             window.append(res)
-#         sequences.append(window)
-#         labels.append(label_map[action])
-# np.array(sequences).shape
-# np.array(labels).shape
-# X = np.array(sequences)
-
-
-# X.shape
-# y = to_categorical(labels).astype(int)
-
-# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.05)
-# y_test.shape
-
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import LSTM, Dense
-# from tensorflow.keras.callbacks import TensorBoard
-# log_dir = os.path.join('Logs')
-# tb_callback = TensorBoard(log_dir=log_dir)
-
-# model = Sequential()
-# model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30,1662)))
-# model.add(LSTM(128, return_sequences=True, activation='relu'))
-# model.add(LSTM(64, return_sequences=False, activation='relu'))
-# model.add(Dense(64, activation='relu'))
-# model.add(Dense(32, activation='relu'))
-# model.add(Dense(actions.shape[0], activation='softmax'))
-# res = [.7, 0.2, 0.1]
-# actions[np.argmax(res)]
-# model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-# model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback])
-# model.summary()
