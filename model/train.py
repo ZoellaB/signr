bfc217bb5b1e200a6ff3c64451ad77e57a2c338e
ZoellaB <zayced@gmail.com>
2025-03-24 15:22:49 -0700

    Multiprocessing done

--- model/train.py
+++ model/train.py
@@ -6,9 +6,8 @@ import time
 import mediapipe as mp
 import json
 import sys
-from multiprocessing import Pool, Manager, Array
+from multiprocessing import Pool
 # from sklearn.model_selection import train_test_split
-from shared_memory_dict import SharedMemoryDict
     
 mp_holistic = mp.solutions.holistic
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -17,7 +16,9 @@ TRAIN_DIR = "training_data"
 GEN_DIR = 'gen_keypoints_data'
 START = 0
 KEYPTS_PATH = os.path.join(os.curdir, GEN_DIR)
-word_class = SharedMemoryDict(name='tokens',size=2048)
+
+word_class = None
+missing = None
 
 def mediapipe_detect(img, model):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -59,7 +60,7 @@ def get_keypts(results):
 
 def get_word_classification(dir):
     #Save word classification from 0-1999 as dict
-    
+    word_class = {}
     with open(TRAIN_DIR + dir) as class_file:
         for line in class_file:
             if (len(line.split(maxsplit=1)) != 2):
@@ -69,12 +70,12 @@ def get_word_classification(dir):
     return word_class
 
 def get_missing_vids(miss_dir):
-    missing = Array('i', size_or_initializer=2000, lock=False)
-    missing = open(TRAIN_DIR + miss_dir).readlines()  #Save missing video instances
-    return missing
+    miss = []
+    miss = open(TRAIN_DIR + miss_dir).readlines()  #Save missing video instances
+    return miss
 
 def get_vid_metadata(dir):
-    train_list = Array('c',size_or_initializer=2000, lock=False)
+    train_list = []
     
     # Open the JSON file
     with open(TRAIN_DIR + dir) as f:
@@ -88,66 +89,63 @@ def get_vid_metadata(dir):
     else: # may break here.
         print("The JSON file does not contain an array.")
 
-    print(len(train_list))
     return train_list
 
-def get_keypts_orchestrator(train_list):
+def get_keypts_orchestrator(word):
     # Trains per word, go to each 1+ instances
-    for word in train_list:
-        print("Training " + word['gloss'])
-        for instance in word['instances']:
-            try: 
-                os.makedirs(os.path.join(KEYPTS_PATH, word_class[str(word['gloss'])], str(instance['video_id'])))
-            except:
-                pass
+    print("Training " + word['gloss'])
+    for instance in word['instances']:
+        try: 
+            os.makedirs(os.path.join(KEYPTS_PATH, word_class[str(word['gloss'])], str(instance['video_id'])))
+        except:
+            pass
 
-            if instance['video_id'] not in missing:
-                with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5) as holistic:
-                    cap = cv2.VideoCapture('training_data/videos/' + instance['video_id'] +  '.mp4')
-                    frame_count = 0
-                    while cap.isOpened():
-                        ret, frame = cap.read()
-                        if not ret:
-                            print("Ignoring empty camera frame.")
-                            break 
-                        img, results = mediapipe_detect(frame, holistic)
-                        frame_count += 1
+        if instance['video_id'] not in missing:
+            with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5) as holistic:
+                cap = cv2.VideoCapture('training_data/videos/' + instance['video_id'] +  '.mp4')
+                frame_count = 0
+                while cap.isOpened():
+                    ret, frame = cap.read()
+                    if not ret:
+                        print("Ignoring empty camera frame.")
+                        break 
+                    img, results = mediapipe_detect(frame, holistic)
+                    frame_count += 1
 
-                        # #Showing, not neccesary when training
-                        # draw_landmarks(img, results)
-                        # cv2.imshow('Cam Feed', img)
+                    # #Showing, not neccesary when training
+                    # draw_landmarks(img, results)
+                    # cv2.imshow('Cam Feed', img)
 
-                        #Saving keypoints:
-                        keypts = get_keypts(results)
-                        numpy_path = os.path.join(KEYPTS_PATH, word_class[str(word['gloss'])], str(instance['video_id']),str(frame_count))
-                        if not os.path.exists(numpy_path):
-                            np.save(numpy_path, keypts)
-        
-                        if (cv2.waitKey(1) & 0xFF == ord('q')):
-                            continue
-                    cap.release()
+                    #Saving keypoints:
+                    keypts = get_keypts(results)
+                    numpy_path = os.path.join(KEYPTS_PATH, word_class[str(word['gloss'])], str(instance['video_id']),str(frame_count))
+                    if not os.path.exists(numpy_path):
+                        np.save(numpy_path, keypts)
+    
+                    if (cv2.waitKey(1) & 0xFF == ord('q')):
+                        continue
+                cap.release()
     cv2.destroyAllWindows()
 
-if not os.path.exists(KEYPTS_PATH):
-    os.makedirs(KEYPTS_PATH)
-    print("hey")
+
+def init_worker(wc_dir, missword_dir):
+    global word_class, missing
+    word_class = get_word_classification(wc_dir)
+    missing = get_missing_vids(missword_dir)
 
 def main():
+    global word_class, missing
 
-    
+    if not os.path.exists(KEYPTS_PATH):
+        os.makedirs(KEYPTS_PATH)
 
-    word_class = get_word_classification("/wlasl_class_list.txt")
-    missing = get_missing_vids("/missing.txt")
-    tl = get_vid_metadata("/WLASL_v0.3.json")
+    train_list = get_vid_metadata("/WLASL_v0.3.json")
+    with Pool(initializer=init_worker, initargs=("/wlasl_class_list.txt", "/missing.txt")) as pool:
+        pool.imap_unordered(get_keypts_orchestrator,train_list)
+        pool.close()
+        pool.join()
 
-    pool = Pool()
-    with Pool() as pool:
-        print("way")
-        pool.imap_unordered(get_keypts_orchestrator, [(i, tl) for i in range(len(tl))])
-        print("ney")
+
 
 if __name__ == '__main__':
     main()
-
-word_class.shm.close()
-word_class.shm.unlink()
